1ab71d5098cbb73759f2205e0491e022d82ac6b9
RbertoVil <rantoniovillasana@gmail.com>
2021-05-11 13:51:14 -0400

    Add second loop to my python practice

--- 5-semester/practices/python/counter/main.py
+++ 5-semester/practices/python/counter/main.py
@@ -4,16 +4,20 @@ import csv
 
 def main():
     contador = 0
-    while (contador < 5):
-        with open("file.csv", mode='a') as writer:
-            time.sleep(60)
+    dia = 0
+    while (dia < 2):
+        for i in range(5):
+            with open("file.csv", mode='a') as writer:
+                time.sleep(60)
 
-            today = datetime.datetime.now()
-            fecha = f"{ today.day } - { today.month } - { today.year }"
-            hora = f"{ today.hour }:{ today.minute }"
+                today = datetime.datetime.now()
+                fecha = f"{ today.day } - { today.month } - { today.year }"
+                hora = f"{ today.hour }:{ today.minute }"
 
-            contador += 1
-            print(f"Lleva { contador } minutos. Durante fecha: { fecha } y hora: { hora }", file=writer)
+                contador += 1
+                print(f"Dia { dia }. Lleva { contador } minutos. Durante fecha: { fecha } y hora: { hora }", file=writer)
+        
+        dia += 1
 
     print(f"Se ha finalizado el conteo, en total pasaron { contador } minutos.")
 
