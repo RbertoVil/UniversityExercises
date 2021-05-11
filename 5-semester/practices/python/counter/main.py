01a3851af434875b1ece2202d232b28393bcc229
RbertoVil <rantoniovillasana@gmail.com>
2021-05-11 12:15:59 -0400

    Add a writer object file to my python practice

--- 5-semester/practices/python/counter/main.py
+++ 5-semester/practices/python/counter/main.py
@@ -4,9 +4,10 @@ import csv
 def main():
     contador = 0
     while (contador < 5):
-        time.sleep(60)
-        contador += 1
-        print(f"Lleva { contador } minutos.")
+        with open("file.csv", mode='a') as writer:
+            time.sleep(60)
+            contador += 1
+            print(f"Lleva { contador } minutos.", file=writer)
 
     print(f"Se ha finalizado el conteo, en total pasaron { contador } minutos.")
 
