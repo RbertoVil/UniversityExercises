521fea8174ffed2591d8552ba307f40d53404019
RbertoVil <rantoniovillasana@gmail.com>
2021-05-11 09:07:52 -0400

    Add a basic counter to my practice

--- 5-semester/practices/python/counter/main.py
+++ 5-semester/practices/python/counter/main.py
@@ -3,9 +3,12 @@ import csv
 
 def main():
     contador = 0
+    while (contador < 5):
+        time.sleep(60)
+        contador += 1
+        print(f"Lleva { contador } minutos.")
 
-
-
+    print(f"Se ha finalizado el conteo, en total pasaron { contador } minutos.")
 
 
 
